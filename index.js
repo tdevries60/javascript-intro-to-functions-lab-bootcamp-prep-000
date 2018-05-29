3ef6d90178a22ea3a48babd82024d45999b8e641
Tom De Vries <tdevries60@gmail.com>
2018-05-29 18:34:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
+  else if (string === "I love you, Grandma.") {
+    return "I love you too, Grandma."
+  }
 }
