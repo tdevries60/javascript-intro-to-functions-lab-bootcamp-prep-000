3e1d0984a3358cb33580559ef01897adaf688fdc
Tom De Vries <tdevries60@gmail.com>
2018-05-29 17:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function shout(string) {
  function whisper(string) {
    return string.toLowerCase()
  }
+
 function sayHiToGrandma(string) {
   if string.toLowerCase === lowercase {
     console.log("I can't hear you!")
